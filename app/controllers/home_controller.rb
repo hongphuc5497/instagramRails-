81d9957fc7e4117cc593f1b0c121e8ccc1981982
hongphuc <hongphuc.dthp@gmail.com>
2019-07-19 13:44:19 +0700

    show post details, delete a post

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
   end
 
   def index
-    @posts = Post.order(created_at: :desc).page(params[:page]).per(5)
+    @posts = Post.order(created_at: :desc).page(params[:page]).per(3)
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,19 @@
 class PostsController < ApplicationController
+    def show
+        @post = Post.find(params[:id])
+    end
+
     def create
         Post.create(post_params)
         redirect_to root_path
     end
       
+    def destroy
+        @post = current_user.posts.find(params[:id])
+        @post.destroy
+
+        redirect_to user_path(current_user)
+    end
     private
 
     def post_params
